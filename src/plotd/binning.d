94dafe9078fc09cb8b8b6f1995852a26ecb645cd
BlackEdder <edder@tkwsping.nl>
2014-10-12 11:03:09 +0100

    Rewrite binning module to be more D like

--- src/plotd/binning.d
+++ src/plotd/binning.d
@@ -23,21 +23,50 @@
 
 module plotd.binning;
 
-/**
-  Bins is used for binning data
+import std.array;
+import std.range;
 
-  Default bins are Bins!size_t, you can build multidimensional bins using Bins!Bins!size_t
-  */
-class Bins(T) {
+/**
+  The struct Bins is a container holding binned data
+ */
+struct Bins(T) {
     double min;
     double width;
-    size_t max_size;
 
+    /// First value in the bins
+    T front() {
+        return mybins[0];
+    }
+
+    /// Return first value and drop it
+    void popFront() {
+        min += width;
+        mybins = mybins[1..$];
+    }
+    
+    /// Whether the container contains any bins
+    bool empty() {
+        if (mybins.length>0)
+            return false;
+        else 
+            return true;
+    }
+
+    /// How many bins does the container have
     size_t length() {
         return mybins.length;
     }
+
+    /// save the container position
+    @property Bins!T save() { return this; }
+
+    /// Access bin by index
+    ref T opIndex(size_t index)
+    {
+        return mybins[index];
+    }
     
-    int opApply(int delegate( ref double, ref T ) dg)
+    /*int opApply(int delegate( ref double, ref T ) dg)
     {
         int result;
 
@@ -51,22 +80,55 @@ class Bins(T) {
             x += width;
         }
         return result;
-    }
-
-    Bins!T dup() {
-        auto bins = new Bins!T();
-        bins.min = min;
-        bins.width = width;
-        bins.max_size = max_size;
-        foreach ( el ; mybins )
-            bins.mybins ~= el;
-        return bins;
-    }
+    }*/
 
     private:
         T[] mybins;
 }
 
+/// For loop over Bins
+unittest {
+    Bins!size_t bins;
+    bins.min = -1;
+    bins.width = 0.5;
+    bins.mybins = [1,2,3,4];
+
+    size_t correct_el = 1;
+    foreach ( el; bins ) {
+        assert( correct_el == el );
+        correct_el++;
+    }
+
+    /*double correct_x = bins.min;
+    double correct_el = 1;
+    foreach ( x, el; bins ) {
+        assert( correct_x == x );
+        assert( correct_el == el );
+        correct_x += bins.width;
+        correct_el++;
+    }*/
+}
+
+/**
+  Calculate bin id based on data value and Bins
+
+  TODO implement this for multidimensional bins
+  */
+size_t binId(T)( in Bins!T bins, double data ) {
+    assert( data >= bins.min );
+    return cast(size_t)( (data-bins.min)/bins.width );
+}
+
+unittest {
+    Bins!size_t bins;
+    bins.min = -1;
+    bins.width = 0.5;
+    assert( binId!size_t( bins, -1.0 ) == 0 );
+    assert( binId( bins, -0.5 ) == 1 );
+    assert( bins.binId( -0.25 ) == 1 );
+}
+
+/*
 /// For loop over Bins
 unittest {
     auto bins = new Bins!size_t;
@@ -82,7 +144,7 @@ unittest {
         correct_x += bins.width;
         correct_el++;
     }
-}
+}*/
 
 /**
   Add data to the given bin id
@@ -91,56 +153,48 @@ unittest {
 
   Ignore values that fall outside of the existing bin range
  */
-Bins!T add_data_to_bin( T )( Bins!T bins, const size_t[] bin_ids ) 
+Bins!T addDataToBin( T )( Bins!T bins, const size_t[] binIds ) 
 {
-    if (bin_ids[0] < bins.mybins.length) {
-        bins.mybins[bin_ids[0]].add_data_to_bin( bin_ids[1..$] );
-        if (bins.mybins[bin_ids[0]].max_size > bins.max_size)
-            bins.max_size = bins.mybins[bin_ids[0]].max_size;
+    static if (__traits( compiles, 
+                bins[binIds[0]].addDataToBin( binIds[1..$] ) ) ) {
+        if (binIds[0] < bins.length) {
+            bins[binIds[0]].addDataToBin( binIds[1..$] );
+        }
     }
-    return bins;
-}
-
-Bins!T add_data_to_bin( T : size_t )( Bins!T bins, const size_t[] bin_ids ) 
-{
-    if (bin_ids[0] < bins.mybins.length) {
-        bins.mybins[bin_ids[0]]++;
-        if (bins.mybins[bin_ids[0]] > bins.max_size)
-            bins.max_size = bins.mybins[bin_ids[0]];
+    else {
+        if (binIds[0] < bins.length) {
+            bins[binIds[0]]++;
+        }
     }
     return bins;
 }
 
 unittest {
-    auto bins = new Bins!size_t;
+    Bins!size_t bins;
     bins.min = -1;
     bins.width = 0.5;
-    bins.max_size = 4;
     bins.mybins = [1,2,3,4];
 
-    bins.add_data_to_bin( [1] );
+    bins.addDataToBin( [1] );
     assert( bins.mybins[1] == 3 );
-    bins.add_data_to_bin( [3] );
+    bins.addDataToBin( [3] );
     assert( bins.mybins[3] == 5 );
-    assert( bins.max_size == 5 );
 
-    auto mbins = new Bins!(Bins!size_t);
+    Bins!(Bins!size_t) mbins;
     mbins.min = -1;
     mbins.width = 0.5;
-    mbins.mybins = [bins, bins.dup];
-    mbins.max_size = 5;
-    mbins.add_data_to_bin( [1,2] );
+    mbins.mybins = [bins, bins];
+    mbins.addDataToBin( [1,2] );
     assert( mbins.mybins[1].mybins[2] == 4 );
-    mbins.add_data_to_bin( [1,3] );
+    mbins.addDataToBin( [1,3] );
     assert( mbins.mybins[1].mybins[3] == 6 );
-    assert( mbins.max_size == 6 );
 }
 /**
   Resize the bins
 
   Again an array in case of resizing multidmensional arrays
  */
-Bins!T resize( T )( Bins!T bins, const size_t[] new_length ) {
+/*Bins!T resize( T )( Bins!T bins, const size_t[] new_length ) {
     T default_value;
     assert( bins.mybins.length > 0, 
             "Multidimensional need to have at least one bin to correctly resize" );
@@ -189,13 +243,13 @@ unittest {
 
     foreach( x, bin; mbins )
         assert( bin.length == 8 );
-}
+}*/
 
  
 /**
   Calculate bin id based on data value and Bins
   */
-size_t bin_id(T)( const Bins!T bins, double data ) {
+/*size_t bin_id(T)( const Bins!T bins, double data ) {
     assert( data >= bins.min );
     return cast(size_t)( (data-bins.min)/bins.width );
 }
@@ -206,24 +260,23 @@ unittest {
     assert( bin_id( bins, -1 ) == 0 );
     assert( bin_id( bins, -0.5 ) == 1 );
     assert( bin_id( bins, -0.25 ) == 1 );
-}
+}*/
 /**
   Add data to existing Bins
   */
-Bins!T add_data( T )( Bins!T bins, const double[] data ) {
+/*Bins!T addData( T )( Bins!T bins, const double[] data ) {
     size_t[] ids;
     auto bins_step = bins;
-    writeln( "bla" );
     for ( size_t i = 0; i < data.length; i++ ) {
         ids ~= bin_id( bins_step, data[i] );
         if (i < data.length -1)
             bins_step = bins_step.mybins[0];
     }
-    return add_data_to_bin( bins, ids.reverse );
+    return addDataToBin( bins, ids.reverse );
 }
 
-Bins!T add_data( T : size_t )( Bins!T bins, const double[] data ) {
-    return add_data_to_bin( bins, [bin_id( bins, data[0] )] );
+Bins!T addData( T : size_t )( Bins!T bins, const double[] data ) {
+    return addDataToBin( bins, [bin_id( bins, data[0] )] );
 }
 
 unittest {
@@ -233,9 +286,9 @@ unittest {
     bins.max_size = 4;
     bins.mybins = [1,2,3,4];
 
-    bins = bins.add_data( [-0.25] );
+    bins = bins.addData( [-0.25] );
     assert( bins.mybins[1] == 3 );
-    bins = add_data( bins, [0.75] );
+    bins = addData( bins, [0.75] );
     assert( bins.mybins[3] == 5 );
     assert( bins.max_size == 5 );
 
@@ -244,13 +297,9 @@ unittest {
     mbins.width = 0.5;
     mbins.mybins = [bins, bins.dup];
     mbins.max_size = 5;
-    mbins.add_data_to_bin( [1,2] );
+    mbins.addDataToBin( [1,2] );
     assert( mbins.mybins[1].mybins[2] == 4 );
-    mbins.add_data_to_bin( [1,3] );
+    mbins.addDataToBin( [1,3] );
     assert( mbins.mybins[1].mybins[3] == 6 );
     assert( mbins.max_size == 6 );
-}
-
-/**
-  Bin an array of data
-  */
+}*/
