145d56b64274c2199f48c7238b6ef6fbd01d6aeb
BlackEdder <edder@tkwsping.nl>
2015-12-28 12:45:02 +0000

    Working follow range

--- source/plotcli/parse.d
+++ source/plotcli/parse.d
@@ -1,14 +1,28 @@
 module plotcli.parse;
 
-unittest
+import std.range : isInputRange;
+
+version( unittest )
 {
-    assert( true );
+    import dunit.toolkit;
 }
 
-struct FollowRange(RANGE)
+struct FollowRange(RANGE) if (isInputRange!RANGE)
 {
-    this( RANGE range )
+    import std.range : ElementType;
+    import core.thread : Thread;
+    import core.time : dur;
+
+    this( RANGE range, bool follow = true )
     {
+        import std.range : empty, popFront, front;
+        // TODO add non follow mode, and pollingDelay, timeoutDelay 
+        _range = range;
+        _follow = follow;
+        if (!_range.empty)
+            frontValue = _range.front; 
+        else if (!_follow)
+            eof = true;
     }
 
     @property bool empty() const {
@@ -17,16 +31,67 @@ struct FollowRange(RANGE)
 
     void popFront()
     {
+        import std.range : empty, popFront, front;
         // This one either blocks till next line, or sets "" after timeout
+        if (_range.empty && _follow)
+            Thread.sleep(dur!("msecs")(100));
+
+        if (!_range.empty)
+        {
+            _range.popFront;
+            if (!_range.empty)
+                frontValue = _range.front; // Immediately set front value, 
+                // otherwise
+                // the _range could fill up between our popFront and front call
+            else if (_follow)
+                frontValue = ElementType!RANGE.init;
+            else
+                eof = true;
+        }
     }
 
-    @property ref string front() {
-        return "";
+    @property ref auto front() {
+        return frontValue;
     }
 
 private:
-    bool eof;
+    bool eof = false;
+    bool _follow;
+    RANGE _range;
+    ElementType!RANGE frontValue;
 }
+
+unittest 
+{
+    struct TestRange {
+        @property bool empty() const { return true; }
+        void popFront() {}
+        @property string front() { return "bla"; }
+    }
+
+    auto r = TestRange();
+
+    auto f = FollowRange!TestRange( r );
+
+    assertEqual( f.empty, false );
+    f.popFront;
+    assertEqual( f.front, "" );
+}
+
+unittest 
+{
+    import std.math : isNaN;
+    import std.array : array;
+    import std.range : take;
+    auto f = FollowRange!(double[])( [1.0,2.0,3.0] );
+
+    assertEqual([1.0,2.0,3.0], f.take(3).array);
+    assert(isNaN(f.take(4).array[3]));
+
+    auto f2 = FollowRange!(double[])( [1.0,2.0,3.0], false );
+    assertEqual([1.0,2.0,3.0], f2.array);
+}
+
 
 auto readFileByLine()
 {
