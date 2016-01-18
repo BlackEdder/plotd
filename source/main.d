2bb606974c7891ce4d4c0ef7a284ef150fdfb431
BlackEdder <edder@tkwsping.nl>
2016-01-18 10:15:36 +0000

    Fix whitespace

--- source/main.d
+++ source/main.d
@@ -20,18 +20,20 @@ void main(string[] args)
 
     Options options;
 
-    options = updateOptions( options, args[1 .. $] );
+    options = updateOptions(options, args[1 .. $]);
     int maxCol = 0;
     // Also keep track of row number?
     int lineCount = 0;
     import std.range : Appender;
     import ggplotd.colour : ColourID;
     import ggplotd.ggplotd : GGPlotD;
+
     Appender!(Tuple!(double, "x", double, "y", ColourID, "colour")[]) aes;
     GGPlotD gg;
-    foreach( msg; readStdinByLine( false ) ) {
-        options = updateOptions( options, msg );
-        maxCol = reduce!("max(a,b)")(1,options.xColumns ~ options.yColumns);
+    foreach (msg; readStdinByLine(false))
+    {
+        options = updateOptions(options, msg);
+        maxCol = reduce!("max(a,b)")(1, options.xColumns ~ options.yColumns);
         msg = msg.stripComments;
         auto cols = msg.toRange.array;
         // REFACTOR: Implement a valid msg given Options function
@@ -39,23 +41,21 @@ void main(string[] args)
         if (allCols.empty)
             allCols = [0];
 
-        if (cols.length > maxCol && cols.areNumeric( allCols )) {
+        if (cols.length > maxCol && cols.areNumeric(allCols))
+        {
 
             // REFACTOR: IDS to Numeric Values (given options.xColumns/options.yColumns, cols, max(xs,ys), lineCount)
             double[] xs;
             if (!options.xColumns.empty)
                 xs = options.xColumns.map!((a) => cols[a].to!double).array;
-        else
-                xs = (to!double(lineCount)).repeat( options.yColumns.length ).array;
+            else
+                xs = (to!double(lineCount)).repeat(options.yColumns.length).array;
             auto ys = options.yColumns.map!((a) => cols[a].to!double).array;
 
             // Build tuples
-            foreach( i, x; xs )
-                aes.put( 
-                Tuple!(double, "x", double, "y", ColourID, "colour")
-                ( x, ys[i], ColourID(i) ) );
-
-
+            foreach (i, x; xs)
+                aes.put(Tuple!(double, "x", double, "y", ColourID,
+                    "colour")(x, ys[i], ColourID(i)));
 
             //msg.writeln;
             // Should only increase if actual data
@@ -63,6 +63,7 @@ void main(string[] args)
         }
     }
     import ggplotd.geom : geomLine;
-    gg.put( geomLine!(typeof(aes.data))( aes.data ) );
-    gg.save( "plotcli.png" );
+
+    gg.put(geomLine!(typeof(aes.data))(aes.data));
+    gg.save("plotcli.png");
 }
--- source/plotcli/parse.d
+++ source/plotcli/parse.d
@@ -3,7 +3,7 @@ module plotcli.parse;
 import std.range : isInputRange;
 import std.regex : ctRegex;
 
-version( unittest )
+version (unittest)
 {
     import dunit.toolkit;
 }
@@ -14,23 +14,26 @@ struct FollowRange(RANGE) if (isInputRange!RANGE)
     import core.thread : Thread;
     import core.time : dur;
 
-    this( RANGE range, bool follow = true )
+    this(RANGE range, bool follow = true)
     {
         import std.range : empty, front;
+
         // TODO add non follow mode, and pollingDelay, timeoutDelay 
         _range = range;
         _follow = follow;
         if (!_range.empty)
-            frontValue = _range.front; 
+            frontValue = _range.front;
         else if (!_follow)
             eof = true;
     }
 
-    @property bool empty() const {
+    @property bool empty() const
+    {
         return eof;
     }
 
-    @property ref auto front() {
+    @property ref auto front()
+    {
         return frontValue;
     }
 
@@ -40,6 +43,7 @@ struct FollowRange(RANGE) if (isInputRange!RANGE)
             assert(0, "FollowRange is empty");
 
         import std.range : empty, popFront, front;
+
         // This one either blocks till next line, or sets "" after timeout
         if (_range.empty && _follow)
             Thread.sleep(dur!("msecs")(100));
@@ -51,9 +55,9 @@ struct FollowRange(RANGE) if (isInputRange!RANGE)
             _range.popFront;
             if (!_range.empty)
                 frontValue = _range.front; // Immediately set front value, 
-                // otherwise
-                // the _range could fill up between our popFront and front call
-            else if (_follow)
+            // otherwise
+            // the _range could fill up between our popFront and front call
+        else if (_follow)
                 frontValue = ElementType!RANGE.init;
             else
                 eof = true;
@@ -67,63 +71,83 @@ private:
     ElementType!RANGE frontValue;
 }
 
-unittest 
+unittest
 {
-    struct TestRange {
-        @property bool empty() const { return true; }
-        void popFront() {}
-        @property string front() { return "bla"; }
+    struct TestRange
+    {
+        @property bool empty() const
+        {
+            return true;
+        }
+
+        void popFront()
+        {
+        }
+
+        @property string front()
+        {
+            return "bla";
+        }
     }
 
     auto r = TestRange();
 
-    auto f = FollowRange!TestRange( r );
+    auto f = FollowRange!TestRange(r);
 
-    assertEqual( f.empty, false );
+    assertEqual(f.empty, false);
     f.popFront;
-    assertEqual( f.front, "" );
+    assertEqual(f.front, "");
 }
 
-unittest 
+unittest
 {
     import std.math : isNaN;
     import std.array : array;
     import std.range : take;
-    auto f = FollowRange!(double[])( [1.0,2.0,3.0] );
 
-    assertEqual([1.0,2.0,3.0], f.take(3).array);
+    auto f = FollowRange!(double[])([1.0, 2.0, 3.0]);
+
+    assertEqual([1.0, 2.0, 3.0], f.take(3).array);
     assert(isNaN(f.take(4).array[3]));
 
-    auto f2 = FollowRange!(double[])( [1.0,2.0,3.0], false );
-    assertEqual([1.0,2.0,3.0], f2.array);
+    auto f2 = FollowRange!(double[])([1.0, 2.0, 3.0], false);
+    assertEqual([1.0, 2.0, 3.0], f2.array);
 }
 
-auto readStdinByLine( bool follow = true )
+auto readStdinByLine(bool follow = true)
 {
     struct ReadLines
     {
         import std.stdio : readln;
 
-        @property bool empty() {
+        @property bool empty()
+        {
             import std.range : empty;
-            if (msgs.empty) {
+
+            if (msgs.empty)
+            {
                 auto msg = readln();
                 if (msg.length > 0)
-                    msgs ~= msg[0..$-1]; // Strip linebreak (TODO find standard way)
+                    msgs ~= msg[0 .. $ - 1]; // Strip linebreak (TODO find standard way)
             }
             return msgs.empty;
         }
 
-        @property ref auto front() {
+        @property ref auto front()
+        {
             import std.range : front;
-            if (!initialized) {
+
+            if (!initialized)
+            {
                 initialized = true;
             }
             return msgs.front;
         }
-        
-        void popFront() {
+
+        void popFront()
+        {
             import std.range : popFront;
+
             msgs.popFront;
             // This range behaves different, because stdin can be filled up
             // So we read on a call to empty
@@ -133,7 +157,7 @@ auto readStdinByLine( bool follow = true )
         string[] msgs;
     }
 
-    return FollowRange!ReadLines( ReadLines(), follow );
+    return FollowRange!ReadLines(ReadLines(), follow);
     //return ReadLines();
 }
 
@@ -145,10 +169,10 @@ string[] toRange(string line)
     import std.array : array;
     import std.algorithm : strip;
     import std.regex : regex, replaceFirst, split;
+
     // Cut of any comments
     line = line.replaceFirst(regex("#.*"), "");
-    return line.split(csvRegex).map!((d) => d.strip(' ')).array.filter!("a!=\"\"")
-        .array;
+    return line.split(csvRegex).map!((d) => d.strip(' ')).array.filter!("a!=\"\"").array;
 }
 
 unittest
@@ -163,31 +187,34 @@ unittest
     assert(("nan, 2").toRange == ["nan", "2"]);
 }
 
-bool areNumeric(RANGE)(RANGE r, int[] colIDs) {
+bool areNumeric(RANGE)(RANGE r, int[] colIDs)
+{
     import std.algorithm : all;
     import std.string : isNumeric;
     import std.stdio;
+
     return colIDs.all!((a) => r[a].isNumeric);
-}
-
-unittest 
-{
-    assert( ["1","1.1"].areNumeric( [0,1] ));
-    assert( ["0","1.1"].areNumeric( [0,1] ));
-    assert( !["a","1.1"].areNumeric( [0,1] ) );
-    assert( ["a","1.1"].areNumeric( [1] ) );
-}
-
-auto stripComments( string str )
-{
-    import std.algorithm : until;
-    import std.conv : to;
-    return str.until!((a) => a=="#"[0]).to!string;
 }
 
 unittest
 {
-    assertEqual( "Bla #dflkjaklf".stripComments, "Bla " );
-    assertEqual( "Bla".stripComments, "Bla" );
-    assertEqual( "Bla #dflkjaklf kfdajlf".stripComments, "Bla " );
+    assert(["1", "1.1"].areNumeric([0, 1]));
+    assert(["0", "1.1"].areNumeric([0, 1]));
+    assert(!["a", "1.1"].areNumeric([0, 1]));
+    assert(["a", "1.1"].areNumeric([1]));
+}
+
+auto stripComments(string str)
+{
+    import std.algorithm : until;
+    import std.conv : to;
+
+    return str.until!((a) => a == "#"[0]).to!string;
+}
+
+unittest
+{
+    assertEqual("Bla #dflkjaklf".stripComments, "Bla ");
+    assertEqual("Bla".stripComments, "Bla");
+    assertEqual("Bla #dflkjaklf kfdajlf".stripComments, "Bla ");
 }
