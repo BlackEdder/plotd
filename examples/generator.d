8a6fda0778418773a68c41e3c573dce9ae161552
BlackEdder <edder@tkwsping.nl>
2016-01-31 09:27:39 +0000

    Started working on first example

--- examples/generator.d
+++ examples/generator.d
@@ -1,3 +1,15 @@
+import core.thread : Thread;
+import core.time : dur;
+import std.range : iota;
+import std.stdio : writeln;
+
+import dstats.random;
+
 void main()
 {
+    foreach( i; iota(0,1000) )
+    {
+        writeln( rNorm(0,1), ",", rNorm( -3,1 ) );
+        Thread.sleep( dur!("msecs")( 100 ) );
+    }
 }
